ae11a7613f04291f4cda4ca67b95cbef3f1695cc
Mahalakshmi-Bhaskaran <138985816+Mahalakshmi-Bhaskaran@users.noreply.github.com>
2024-06-28 23:40:14 +0530

    four

--- demoapp/src/main/java/com/example/demoapp/controller/ProductController.java
+++ demoapp/src/main/java/com/example/demoapp/controller/ProductController.java
@@ -3,7 +3,7 @@ package com.example.demoapp.controller;
 public class ProductController {
 
     String trying (String m){
-        System.out.println("hi2");
-        return "hi2";
+        System.out.println("hi4");
+        return "hi4";
     }
 }
